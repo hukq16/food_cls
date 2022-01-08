55b4900458baee1a89eb0f16ed68b6435ad6af79
Keegan Hu <hukq16@gmail.com>
2022-01-08 23:45:22 +0800

    fix some bug to run train.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-root = '/root/ML'
+root = '/mnt/disk1/hkq/food_cls'
 log_path = './log/log.txt'
 resume = None
 
 gpu = 0
 num_classes = 101
 lr = 0.1
-batch_size = 128
+batch_size = 256
 weight_decay = 2e-4
 num_epochs = 100
 momentum = 0.9
