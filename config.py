9e2b4f5cb17e1b2ae4425ac3bf4d184502261547
Keegan Hu <hukq16@gmail.com>
2022-01-10 08:41:19 +0800

    add EfficientNet

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ resume = None
 
 gpu = 0
 num_classes = 101
-lr = 0.1
-batch_size = 64
+lr = 0.01
+batch_size = 32
 weight_decay = 2e-4
-num_epochs = 300
+num_epochs = 150
 momentum = 0.9
 cos = False
